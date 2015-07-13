1a447debb52e811fe1a424ec724415d8c6b725e4
Mark Neisler <mark.neisler@hotmail.com>
2015-07-13 16:54:07 -0600

    Initial commit of placeholder helloworld project

---
+++ src/main/java/instructure/technical/test/App.java
@@ -0,0 +1,16 @@
+package instructure.technical.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public App() {
+		
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
