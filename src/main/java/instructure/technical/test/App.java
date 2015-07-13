6596df60dae98ca941421b63b76c90f3bfa4ff61
Mark Neisler <mark.neisler@hotmail.com>
2015-07-13 17:58:58 -0600

    app a json properties file to set input directory. Add gson library to help with parsing maps from apache csv lib

--- src/main/java/instructure/technical/test/App.java
+++ src/main/java/instructure/technical/test/App.java
@@ -1,12 +1,22 @@
 package instructure.technical.test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 
 /**
  * Hello world!
@@ -14,15 +24,34 @@ import org.apache.commons.csv.CSVRecord;
  */
 public class App 
 {
+	protected final Map<String, Object> props = new HashMap<String, Object>();
 	public App() {
+		Gson gson = new Gson();
 		try {
-			Reader in = new FileReader("path/to/file.csv");
-			try {
-				Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			getPropertiesFromClasspath("app.properties.json");
+			String InputFilesDirectory=(String) props.get("InputFilesDirectory");
+			System.out.println("InputFilesDirectory = "+InputFilesDirectory);
+			File[] files = new File(InputFilesDirectory).listFiles();
+			for (File file : files) {
+				Reader in = new FileReader(file);
+				try {
+					Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(in);
+					for (CSVRecord record : records) {
+						HashMap<String, Object> recordWrapper = gson.fromJson(gson.toJson(record),new TypeToken<HashMap<String, Object>>() {
+						}.getType());
+						
+						HashMap<String, Object> recordHm=gson.fromJson(gson.toJson(recordWrapper.get("mapping")),new TypeToken<HashMap<String, Object>>() {
+						}.getType());;
+						System.out.println("****** Record *******");
+						System.out.println(gson.toJson(recordHm));
+					    
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -30,6 +59,43 @@ public class App
 	}
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       App app = new App();
+    }
+    
+    /**
+     * loads properties file from classpath
+     * 
+     * @param propFileName
+     *          the property file name
+     * @return the properties if found.
+     */
+    private synchronized void getPropertiesFromClasspath(String propFileName) {
+
+      try {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(propFileName);
+
+        if (inputStream == null) {
+          System.out.println("property file '" + propFileName + "' not found in the classpath");
+        } else {
+
+          Type typ = new TypeToken<Map<String, Object>>() {
+          }.getType();
+          JsonReader reader = new JsonReader(new InputStreamReader(inputStream, "UTF-8"));
+
+          // read input file as lenient JSON with C-style comments
+          reader.setLenient(true);
+          Map<String, Object> myProps = new Gson().fromJson(reader, typ);
+
+          props.putAll(myProps);
+        
+        
+         
+        }
+
+      } catch (Throwable ex) {
+      
+        System.out.println("Error Loading property file'" + propFileName + "'.");
+        ex.printStackTrace();
+      }
     }
 }
