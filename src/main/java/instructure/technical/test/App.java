70c474b16403349404642eb2c5acf053cb05ed43
Mark Neisler <mark.neisler@hotmail.com>
2015-07-13 16:57:55 -0600

    add apache commons csv library

--- src/main/java/instructure/technical/test/App.java
+++ src/main/java/instructure/technical/test/App.java
@@ -1,5 +1,13 @@
 package instructure.technical.test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 /**
  * Hello world!
  *
@@ -7,7 +15,18 @@ package instructure.technical.test;
 public class App 
 {
 	public App() {
-		
+		try {
+			Reader in = new FileReader("path/to/file.csv");
+			try {
+				Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
     public static void main( String[] args )
     {
